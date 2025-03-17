c5bb1f0717b62aa92bbf381af1162109281b99dc
DAUDU QAZIM <yetkemsupper@gmail.com>
2025-03-17 15:55:11 +0100

    docker

--- obj/Debug/net8.0/VoiceInfo.AssemblyInfo.cs
+++ obj/Debug/net8.0/VoiceInfo.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("18fbc99f-32a6-4007-989b-df928d226571")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoiceInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1414ff158dbc6fba62c7ee9e151d913d5ceaa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cfd0cc5fca38aac98e1b0749c95082f8b1e998")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoiceInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoiceInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
